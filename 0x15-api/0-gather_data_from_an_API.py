74e38a0428fe54d575daca177c3c27d753c08dfd
thiago1623 <thiago.96t16@gmail.com>
2021-02-01 16:31:49 -0500

    [UPDATE]--FILES--(add new changes)

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""
-Calls an API in order to get completed tasks
-"""
+""" Calls an API in order to get completed tasks """
 import requests
 import sys
 
 
 if __name__ == '__main__':
     userId = sys.argv[1]
-    url_TODO = 'https://jsonplaceholder.typicode.com/todos/'
-    url_usr = 'https://jsonplaceholder.typicode.com/users/'
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': userId})
+    user = requests.get(url_user, params={'id': userId})
 
-    todo = requests.get(url_TODO, params={'userID': userId})
-    user = requests.get(url_usr, params={'id': userId})
+    todo_dict = todo.json()
+    user_dict = user.json()
 
-    """convert in json"""
-    dict_todo = todo.json()
-    dict_usr = user.json()
+    completed_tasks = []
+    total_tasks = len(todo_dict)
+    employee = user_dict[0].get('name')
 
-    completed_task = []
-    total_task = len(dict_todo)
-    employee = dict_usr[0].get('name')
-
-    """ add tasks """
-    for task in dict_todo:
+    for task in todo_dict:
         if task['completed']:
-            completed_task.append(task)
+            completed_tasks.append(task)
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee, len(completed_task), total_task))
+          .format(employee, len(completed_tasks), total_tasks))
 
-    for task in completed_task:
+    for task in completed_tasks:
         print("\t {}".format(task.get('title')))
